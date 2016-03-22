d937134492e015109855d865c7adc26ac74444d8
Kim Røen <kim@kimroen.com>
2016-03-22 20:12:47 +0100

    Add trial test for adapters
    This should fial, but it doesn't. What.

---
+++ node-tests/blueprints/adapter-test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
+var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
+var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+
+describe('Acceptance: ember generate and destroy adapter', function() {
+  setupTestHooks(this);
+
+  it('adapter foo', function() {
+    return generateAndDestroy(['adapter', 'foo'], {
+      files: [
+        {
+          file: 'app/adapters/foo.coffee',
+          contents: [
+            "`import ApplicationAdapter from './application'`"
+          ]
+        }
+      ]
+    });
+  });
+
+  it('adapter foo', function() {
+    return generateAndDestroy(['adapter', 'foo'], {
+      files: [
+        {
+          file: 'app/adapters/foo.coffee',
+          contents: [
+            "`import DS from 'ember-data'`"
+          ]
+        }
+      ]
+    });
+  });
+
+});
